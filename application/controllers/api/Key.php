7b98079fc94642829b77cf155989b128e749e8d6
Sofyan Thayf <sofyanthayf@gmail.com>
2019-06-18 09:58:28 +0800

    complete

--- application/controllers/api/Key.php
+++ application/controllers/api/Key.php
@@ -5,7 +5,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 // This can be removed if you use __autoload() in config.php OR use Modular Extensions
 /** @noinspection PhpIncludeInspection */
-//require APPPATH . '/libraries/REST_Controller.php';
+// require APPPATH . '/libraries/REST_Controller.php';
+require APPPATH . 'libraries/REST_Controller.php';
+require APPPATH . 'libraries/Format.php';
 
 /**
  * Keys Controller
